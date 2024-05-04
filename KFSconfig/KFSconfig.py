cfa0062733901dcfac17576843e52a378f70fa8f
9FS <pray4spam@googlemail.com>
2024-05-04 17:43:28 +0000

    complete rewrite of load_config(...), added support for environmental variables and added prioritised multiple config sources; upgraded to python3.12; force_input(...) now interprets -1 as infinite tries

--- KFSconfig/KFSconfig.py
+++ KFSconfig/KFSconfig.py
@@ -1,18 +1,21 @@
-# Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+# Copyright (c) 2024 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
 import inspect
+import json
 import logging
 import os
-from KFSlog import KFSlog
+import typing
+from KFSfstr import KFSfstr
+from KFSlog  import KFSlog
 
 
-def force_input(prompt: str, inputs_allowed: list[str]=["y", "n"], tries_max: int=0, match_case_sensitive: bool=False) -> str:
+def force_input(prompt: str, inputs_allowed: list[str]=["y", "n"], tries_max: int=-1, match_case_sensitive: bool=False) -> str:
     """
     Forces user to input a string from a list of allowed strings. If case_sensitive is false, input matching is case-insensitive but case of inputs_allowed is returned. If user input is not in inputs_allowed and tries_max has been exhausted, raises ValueError.
 
     Arguments:
     - prompt: message to (repeatedly) display to user prompting for input
     - input_allowed: list of allowed user input
-    - tries_max: maximum number of tries allowed before raising ValueError, 0 for infinite
+    - tries_max: maximum number of tries allowed before raising ValueError, -1 for infinite
     - match_case_sensitive: input evaluation case-sensitive or not
 
     Returns:
@@ -32,9 +35,6 @@ def force_input(prompt: str, inputs_allowed: list[str]=["y", "n"], tries_max: in
         logger=logging.getLogger("")            # also use root logger to match formats defined outside KFS
     else:                                       # if no root logger defined:
         logger=KFSlog.setup_logging("KFS")      # use KFS default format
-
-    if tries_max==0:    # if infinite tries:
-        tries_max=-1    # set tries_max to -1 to make while loop run infinitely
 
 
     while try_current!=tries_max:   # as long as tries not exhausted: prompt for input
@@ -59,27 +59,28 @@ def force_input(prompt: str, inputs_allowed: list[str]=["y", "n"], tries_max: in
     return input_allowed_matched
 
 
-def load_config(filepath: str, default_content: str="", empty_ok: bool=False, encoding="utf8") -> str:
+def load_config(config_default: dict[str, typing.Any], env: bool=True, config_filepaths: list[str]|None=["./.env", "./config/config.json"], setting_None_ok: bool=False) -> dict[str, typing.Any]:
     """
-    Tries to load text content from \"filepath\" and return it. If file does not exist, tries to create new file with default_content and then raises FileNotFoundError. If file does exist and is empty, may raise ValueError depending on empty_ok.
+    Tries to load config with the name of the config_default's keys. Multiple sources can be used and will be prioritised accordingly:
+    1. if env is True: passed environemental variables from os.environ
+    2. if config_filepath is not None: config from file at filepath with decreasing priority
+    If any setting is still None after all sources have been tried and setting_None_ok is false, it is considered undefined and a ValueError is raised.
+    If any setting is undefined and none of the enabled filepaths exist, creation of a default config file at the highest priority filepath will be offered using config_default's values.
 
     Arguments:
-    - filepath: filepath to config file to read from or to create
-    - defeault_content: if file at filepath does not exist: default config file content to use
-    - empty_ok: if config file is empty: return empty string or raise ValueError
-    - encoding: encoding to use when reading and writing config file, more at https://docs.python.org/3/library/codecs.html#standard-encodings
+    - config_default: defaults to use for creation of a default config file
+    - env: use passed environmental variables from os.environ?
+    - config_filepath: filepath to config files or None if unused
+    - setting_None_ok: allow settings to be None or require to be set?
 
     Returns:
-    - filecontent: config file content
+    - config: loaded config, same keys as config_default
 
     Raises:
-    - FileNotFoundError: File at filepath does not exist, but has been created and filled with default_content now.
-    - IsADirectoryError: File at filepath does not exist, because it is a directory. 
-    - ValueError: File at filepath does exist, but empty_ok is false and file is empty.
+    - ValueError: After going through all enabled sources, a setting is still None and setting_None_ok is false.
     """
 
-    filecontent: str
-    logger: logging.Logger  # logger
+    config: dict[str, typing.Any]={key: None for key in config_default.keys()}  # loaded config, initialised empty with same keys as config_default
     
 
     if 1<=len(logging.getLogger("").handlers):  # if root logger defined handlers:
@@ -87,39 +88,143 @@ def load_config(filepath: str, default_content: str="", empty_ok: bool=False, en
     else:                                       # if no root logger defined:
         logger=KFSlog.setup_logging("KFS")      # use KFS default format
 
+    if env==False and (config_filepaths==None or len(config_filepaths)==0): # if no source enabled: error
+        logger.error(f"No config source is enabled. Loading config is impossible.")
+        raise ValueError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: No config source is enabled. Loading config is impossible.")
 
-    if os.path.isfile(filepath)==False and os.path.isdir(filepath)==False:  # if input configuration file does not exist yet: create a default one, print instructions with error
-        logger.error(f"Loading \"{filepath}\" is not possible, because file does not exist.")
-        logger.info(f"Creating default \"{filepath}\"...")
-        try:
-            if os.path.dirname(filepath)!="":
-                os.makedirs(os.path.dirname(filepath), exist_ok=True)   # create folders
-            with open(filepath, "wt") as file:                          # create file
-                file.write(default_content)                             # fill with default content
-        except OSError:
-            logger.error(f"\rCreating default \"{filepath}\" failed.")
-            raise FileNotFoundError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Loading \"{filepath}\" is not possible, because file does not exist. Creating default \"{filepath}\" failed.")
-        else:
-            logger.info(f"\rCreated default \"{filepath}\".")
-            raise FileNotFoundError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Loading \"{filepath}\" is not possible, because file did not exist. Created default \"{filepath}\".")
+
+    if env==True:
+        config.update({k: v for k, v in _load_env().items() if k in config.keys() and config[k]==None}) # update config with environmental variables, only overwrite if key already exists and value is None
     
-    elif os.path.isfile(filepath)==False and os.path.isdir(filepath)==True: # if input configuration file is a directory: can't do anything, error
-        logger.error(f"Loading \"{filepath}\" is not possible, because it is a directory. Unable to create default file.")
-        raise IsADirectoryError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Loading \"{filepath}\" is not possible, because it is a directory. Unable to create default file.")
+    if config_filepaths!=None and len(config_filepaths)!=0:
+        for config_filepath in config_filepaths:
+            try:
+                config.update({k: v for k, v in _load_config_file(config_filepath).items() if k in config.keys() and config[k]==None})  # update config with config file, only overwrite if key already exists and value is None
+            except (FileNotFoundError, IsADirectoryError):                                                                              # if fails: ignore source
+                pass
 
 
-    logger.info(f"Loading \"{filepath}\"...")
+    if any(v==None for v in config.values())==True and setting_None_ok==False:                                                                                  # if any setting is still None and not allowed to be None: error
+        logger.error(f"After going through all enabled sources, settings {[k for k, v in config.items() if v==None]} are still None and setting_None_ok is false.")
+        
+        if config_filepaths!=None and 1<=len(config_filepaths) and all([os.path.exists(config_filepath)==False for config_filepath in config_filepaths])==True: # if a file source is enabled and no files at set filepaths exist: offer creation of default config file at highest priority filepath using config_default's values
+            logger.info(f"None of the following filepaths exist: {config_filepaths}")
+            match force_input(f"Would you like to create a default \"{config_filepaths[0]}\"? (y/n)"):
+                case "y":
+                    try:
+                        _create_default_file(config_default, config_filepaths[0])                                                                               # create default config file at highest priority filepath using config_default's values
+                    except OSError:                                                                                                                             # if creating fails: error, if file already exists error because it should have been checked before
+                        pass
+                case "n":
+                    pass
+
+        raise ValueError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: After going through all enabled sources, settings {[k for k, v in config.items() if v==None]} are still None and setting_None_ok is false.")
+
+    return config
+
+def _load_env() -> dict[str, typing.Any]:
+    """
+    Loads the passed environmental variables from os.environ and returns them as a dictionary.
+
+    Returns:
+    - passed environmental variables from os.environ
+    """
+
+    return dict(os.environ)
+
+
+def _load_config_file(config_filepath: str) -> dict[str, typing.Any]:
+    """
+    Loads a config file from the specified filepath and returns its content as a dictionary.
+
+    Arguments:
+    - config_filepath: filepath to config file
+
+    Returns:
+    - config_filecontent: content of config file as a dictionary
+
+    Raises:
+    - FileNotFoundError: config_filepath does not exist
+    - IsADirectoryError: config_filepath is a directory
+    """
+
+    config_filecontent: dict[str, typing.Any]   # content of config file
+
+
+    if 1<=len(logging.getLogger("").handlers):  # if root logger defined handlers:
+        logger=logging.getLogger("")            # also use root logger to match formats defined outside KFS
+    else:                                       # if no root logger defined:
+        logger=KFSlog.setup_logging("KFS")      # use KFS default format
+
+    if os.path.exists(config_filepath)==False:  # if config file does not exist: error
+        logger.error(f"Loading \"{config_filepath}\" failed, because it does not exist.")
+        raise FileNotFoundError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Loading \"{config_filepath}\" failed, because file does not exist.")
+    if os.path.isdir(config_filepath)==True:    # if config file is a directory: error
+        logger.error(f"Loading \"{config_filepath}\" failed, because it is a directory.")
+        raise IsADirectoryError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Loading \"{config_filepath}\" failed, because it is a directory.")
+
+
+    logger.info(f"Loading \"{config_filepath}\"...")
     try:
-        with open(filepath, "rt", encoding=encoding) as file:   # read file
-            filecontent=file.read()
-    except OSError:                                             # write to log, then forward exception
-        logger.error(f"\rLoading \"{filepath}\" failed with OSError.")
+        with open(config_filepath, "rt", encoding="utf8") as config_file:                                                                                                                           # read file
+            match os.path.basename(config_filepath).rsplit(".", 1)[-1]:                                                                                                                             # parse file content
+                case "env":
+                    config_filecontent={line.strip(" ").split("=")[0]: line.strip(" ").split("=")[1] for line in config_file.readlines() if "=" in line and line.strip(" ").startswith("#")==False} # parse env
+                case "json":
+                    config_filecontent=json.loads(config_file.read())                                                                                                                               # parse json
+                case _:
+                    logger.critical(f"\rLoading \"{config_filepath}\" failed, because file extension is not implemented.")
+                    raise NotImplementedError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Loading \"{config_filepath}\" failed, because file extension is not implemented.")
+    except OSError as e:                                                                                                                                                                            # write to log, then forward exception
+        logger.error(f"\rLoading \"{config_filepath}\" failed with {KFSfstr.full_class_name(e)}.")
         raise
     else:
-        logger.info(f"\rLoaded \"{filepath}\".")
+        logger.info(f"\rLoaded \"{config_filepath}\".")
     
-    if filecontent=="" and empty_ok==False:                         # but if content is empty and not allowed empty:
-        logger.error(f"\rLoaded \"{filepath}\", but it is empty.")  # error
-        raise ValueError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Loaded \"{filepath}\", but it is empty.")
+    return config_filecontent
+
+
+def _create_default_file(config_default: dict[str, typing.Any], config_filepath: str) -> None:
+    """
+    Creates a default config file at the specified filepath using config_default.
+
+    Arguments:
+    - config_default: defaults to use for creation of a default config file
+    - config_filepath: filepath to config file
+
+    Raises:
+    - FileExistsError: config_filepath already exists
+    - NotImplementedError: file extension is not implemented yet
+    - OSError: creating config_filepath failed
+    """
+
+    if 1<=len(logging.getLogger("").handlers):  # if root logger defined handlers:
+        logger=logging.getLogger("")            # also use root logger to match formats defined outside KFS
+    else:                                       # if no root logger defined:
+        logger=KFSlog.setup_logging("KFS")      # use KFS default format
+
+    if os.path.exists(config_filepath)==True:   # if config filepath already exists: error
+        logger.error(f"Creating \"{config_filepath}\" is not possible, because it already exists.")
+        raise FileExistsError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Creating \"{config_filepath}\" is not possible, because it already exists.")
     
-    return filecontent
+    
+    logger.info(f"Creating default \"{config_filepath}\"...")
+    try:
+        if os.path.dirname(config_filepath)!="":
+            os.makedirs(os.path.dirname(config_filepath), exist_ok=True)    # create directories
+        with open(config_filepath, "wt", encoding="utf8") as config_file:   # create file
+            match os.path.basename(config_filepath).rsplit(".", 1)[-1]:     # fill with default content
+                case "env":
+                    config_file.write("\n".join([f"{k}={v}" for k, v in config_default.items()]))
+                case "json":
+                    config_file.write(json.dumps(config_default, indent=4))
+                case _:
+                    logger.critical(f"\rCreating default \"{config_filepath}\" failed, because file extension is not implemented.")
+                    raise NotImplementedError(f"Error in {load_config.__name__}{inspect.signature(load_config)}: Creating default \"{config_filepath}\" failed, because file extension is not implemented.")
+    except OSError as e:
+        logger.error(f"\rCreating default \"{config_filepath}\" failed with {KFSfstr.full_class_name(e)}.")
+        raise
+    else:
+        logger.info(f"\rCreated default \"{config_filepath}\".")    
+
+    return
